6913132205ac2d7e63b88b05576c12c0db509ac9
alexseefried <33331375+alexseefried@users.noreply.github.com>
2019-03-21 15:08:37 -0700

    Defined Spawn Locations
    Used game objects to define area for spawning crops and animals.

---
+++ Assets/Scripts/SpawnSpaceAnimals.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSpaceAnimals : MonoBehaviour
+{
+    public Color GizmosColor = new Color(0.5f, 0.5f, 0.5f, 0.2f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //define animal spawn area as vectors in 3D scene
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = GizmosColor;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+
+    }
+
+}
