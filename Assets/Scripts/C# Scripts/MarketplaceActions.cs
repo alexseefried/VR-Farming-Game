0793c61e2ad802fd3847b69e514704c7518aff24
TRUZijunChen <benjamin9527@icloud.com>
2019-03-21 16:32:05 -0700

    Added spown function and cow, pig FBX

--- Assets/Scripts/C# Scripts/MarketplaceActions.cs
+++ Assets/Scripts/C# Scripts/MarketplaceActions.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MarketplaceActions : MonoBehaviour
 {
-
+    public Vector3 animalArea1;
+    public Vector3 animalArea2;
+    public Vector3 cropArea;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,8 @@ public class MarketplaceActions : MonoBehaviour
         if(GetComponent<CurrencySystem>().subtractMoney(150))
         {
             Debug.Log("Bought Cow");
+            animalArea1 = new Vector3(Random.Range(10.19f, 15.52f), 3.5f, Random.Range(5.85f, 10.98f));
+            Instantiate(Resources.Load("Cow"), animalArea1, Quaternion.identity);
         }
         else
         {
@@ -46,7 +50,8 @@ public class MarketplaceActions : MonoBehaviour
         //If true, subtract money from total and add new pig to scene
         if (GetComponent<CurrencySystem>().subtractMoney(150))
         {
-
+            animalArea2 = new Vector3(Random.Range(5.32f, 10.33f), 3.5f, Random.Range(11.79f, 16.70f));
+            Instantiate(Resources.Load("Pig"), animalArea2, Quaternion.identity);
         }
     }
 
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    Vector3 animalArea1;
+    Vector3 animalArea2;
+    Vector3 cropArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnCow()
+    {
+        animalArea1 = new Vector3(Random.Range(10.19f, 15.52f), 4, Random.Range(5.85f, 10.98f));
+        Instantiate(Resources.Load("Cow"), animalArea1, Quaternion.identity);
+    }
+
+    public void spawnPig()
+    {
+        animalArea2 = new Vector3(Random.Range(5.32f, 10.33f), 4, Random.Range(11.79f, 16.70f));
+        Instantiate(Resources.Load("Pig"), animalArea2, Quaternion.identity);
+    }
+
+    public void spawnCorn()
+    {
+        //cropArea = new Vector3(Random.Range(9.7f, 26.6f), 4, Random.Range(27.6f, 34.4f));
+        
+    }
+
+    public void spawnWheat()
+    {
+        //cropArea = new Vector3(Random.Range(9.7f, 26.6f), 4, Random.Range(27.6f, 34.4f));
+
+    }
+}
