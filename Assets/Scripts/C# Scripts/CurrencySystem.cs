ed7553dfb387cbdd9c196f47b47d9bf2da248ba9
Ben Chen <chenbenjamin66@gmail.com>
2019-03-22 12:24:43 -0700

    Phase3 (#4)
    * added Lv1&2 Barn models
    
    * add maya binary files
    
    * add wheat model
    
    * add corn model
    
    * add gallina
    
    * add menus on market place, functions along with market place
    
    * Added spown function and cow, pig FBX
    
    * everything works fine, except skybox

---
+++ Assets/Scripts/C# Scripts/CurrencySystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencySystem : MonoBehaviour
+{
+    public static int currentMoney; //current amount held by user
+    Text money;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        money = GetComponent<Text>();
+        currentMoney = 500; //set default money at start of game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        money.text = "$: " + currentMoney;
+    }
+
+    //used when user sells animals or crops
+    public void addMoney(int moneyAdd)
+    {
+        currentMoney += moneyAdd; //add money to users total
+         //display total money to user
+    }
+
+    //used when user buys animals/crops/buildings
+    public bool subtractMoney(int moneySub)
+    {
+        if(currentMoney - moneySub < 0)
+        {
+            //display message stating that user does not have enough money
+            Debug.Log("Not enough money available to purchase that!");
+            return false;
+        }
+        else
+        {
+            currentMoney -= moneySub; //subtract money from users total
+
+            //display total money to user
+
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/C# Scripts/MarketplaceActions.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketplaceActions : MonoBehaviour
+{
+    public Vector3 animalArea1;
+    public Vector3 animalArea2;
+    public Vector3 cropArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void buyCow()
+    {
+        //If true, subtract money from total and add new cow to scene
+        if(GetComponent<CurrencySystem>().subtractMoney(150))
+        {
+            Debug.Log("Bought Cow");
+            animalArea1 = new Vector3(Random.Range(10.19f, 15.52f), 3.5f, Random.Range(5.85f, 10.98f));
+            Instantiate(Resources.Load("Cow"), animalArea1, Quaternion.identity);
+        }
+        else
+        {
+            //not enough money!
+        }
+        
+    }
+
+    public void buyChicken()
+    {
+        //If true, subtract money from total and add new chicken to scene
+        if (GetComponent<CurrencySystem>().subtractMoney(150))
+        {
+
+        }
+    }
+
+    public void buyPig()
+    {
+        Debug.Log("Bought pig");
+        //If true, subtract money from total and add new pig to scene
+        if (GetComponent<CurrencySystem>().subtractMoney(150))
+        {
+            animalArea2 = new Vector3(Random.Range(5.32f, 10.33f), 3.5f, Random.Range(11.79f, 16.70f));
+            Instantiate(Resources.Load("Pig"), animalArea2, Quaternion.identity);
+        }
+    }
+
+    public void buyCorn()
+    {
+        //If true, subtract money from total and add new corn to scene
+        if (GetComponent<CurrencySystem>().subtractMoney(150))
+        {
+            Debug.Log("Bought Corn");
+        }
+
+    }
+
+    public void buyWheat()
+    {
+        //If true, subtract money from total and add new wheat to scene
+        if (GetComponent<CurrencySystem>().subtractMoney(150))
+        {
+            Debug.Log("Bought Wheat");
+        }
+
+    }
+    
+}
